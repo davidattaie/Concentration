8c7fdf6f7ae65416746412da1b7a127a43c89f67
David Attaie <david@lovemycloset.com>
2018-09-08 20:09:16 -0700

    updated the app to support basic launcher

--- concentration/View Controllers/CONOpeningViewController.m
+++ concentration/View Controllers/CONOpeningViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CONOpeningViewController.h"
+#import "CONGameViewController.h"
 
 @interface CONOpeningViewController ()
 
@@ -14,23 +15,53 @@
 
 @implementation CONOpeningViewController
 
+static CGFloat buttonHeight = 80.0f;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view setBackgroundColor:[UIColor redColor]];
+    [self setupButtonStack];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSArray<UIView *> *)buttons {
+    UIButton *newGameButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [newGameButton setTitle:@"New Game" forState:UIControlStateNormal];
+    [newGameButton addTarget:self action:@selector(launchNewGame:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIButton *previousScoresButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [previousScoresButton setTitle:@"Previous Scores" forState:UIControlStateNormal];
+    
+    return @[newGameButton, previousScoresButton];
 }
-*/
+
+- (void)setupButtonStack {
+    NSArray *buttons = [self buttons];
+    UIStackView *buttonStack = [[UIStackView alloc] initWithArrangedSubviews:buttons];
+    [buttonStack setAlignment:UIStackViewAlignmentCenter];
+    [buttonStack setDistribution:UIStackViewDistributionFillEqually];
+    [buttonStack setAxis:UILayoutConstraintAxisVertical];
+    [self.view addSubview:buttonStack];
+    [self setupConstraintsForButtonStack:buttonStack withNumberOfButtons:buttons.count];
+}
+
+- (void)setupConstraintsForButtonStack:(UIStackView *)buttonStack withNumberOfButtons:(NSInteger)numberOfButtons {
+    [[buttonStack.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor] setActive:YES];
+    [buttonStack setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [[buttonStack.heightAnchor constraintEqualToConstant:numberOfButtons * buttonHeight] setActive:YES];
+    [[buttonStack.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor] setActive:YES];
+    [[buttonStack.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor] setActive:YES];
+    [self.view layoutIfNeeded];
+}
+
+#pragma mark - Button Actions
+
+- (void)launchNewGame:(id)target {
+    CONGameViewController *gameViewController = [CONGameViewController new];
+    [self presentViewController:gameViewController animated:YES completion:nil];
+}
 
 @end
