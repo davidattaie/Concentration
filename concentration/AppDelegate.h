46446104ac4fbab4b49dd06a94bce37826ae5072
David Attaie <david@lovemycloset.com>
2018-09-08 19:29:45 -0700

    Initial Commit

---
+++ concentration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  concentration
+//
+//  Created by David Attaie on 9/8/18.
+//  Copyright © 2018 David Attaie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ concentration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  concentration
+//
+//  Created by David Attaie on 9/8/18.
+//  Copyright © 2018 David Attaie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
